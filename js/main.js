507c9f18fa6e01243754b3a7ee9c97c49a30554d
Arun Deegutla <arundeegutla2004@gmail.com>
2022-07-11 22:04:41 +0530

    index

--- js/main.js
+++ js/main.js
@@ -83,24 +83,24 @@
 	
 
 
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		// 	 timer;
-		// clearTimeout(timer);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			// timer;
-		// timer = setTimeout(function(){
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
-			$this.find('.dropdown-menu').removeClass('show');
-		// }, 100);
-	});
+	// $('nav .dropdown').hover(function(){
+	// 	var $this = $(this);
+	// 	// 	 timer;
+	// 	// clearTimeout(timer);
+	// 	$this.addClass('show');
+	// 	$this.find('> a').attr('aria-expanded', true);
+	// 	// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
+	// 	$this.find('.dropdown-menu').addClass('show');
+	// }, function(){
+	// 	var $this = $(this);
+	// 		// timer;
+	// 	// timer = setTimeout(function(){
+	// 		$this.removeClass('show');
+	// 		$this.find('> a').attr('aria-expanded', false);
+	// 		// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
+	// 		$this.find('.dropdown-menu').removeClass('show');
+	// 	// }, 100);
+	// });
 
 
 	$('#dropdown04').on('show.bs.dropdown', function () {
@@ -115,37 +115,37 @@
 					navbar = $('.ftco_navbar'),
 					sd = $('.js-scroll-wrap');
 
-			if (st > 0) {
+			if (st > 50) {
 				if ( !navbar.hasClass('scrolled') ) {
 					navbar.addClass('scrolled');	
 				}
 			} 
-			if (st < 0) {
+			if (st <= 50) {
 				if ( navbar.hasClass('scrolled') ) {
 					navbar.removeClass('scrolled sleep');
 				}
 			} 
-			if ( st > 0 ) {
+			if ( st > 50 ) {
 				if ( !navbar.hasClass('awake') ) {
 					navbar.addClass('awake');	
 				}
 				
-				if(sd.length > 0) {
+				if(sd.length > 50) {
 					sd.addClass('sleep');
 				}
 			}
-			if ( st < 0 ) {
+			if ( st <= 50 ) {
 				if ( navbar.hasClass('awake') ) {
 					navbar.removeClass('awake');
 					navbar.addClass('sleep');
 				}
-				if(sd.length > 0) {
+				if(sd.length > 50) {
 					sd.removeClass('sleep');
 				}
 			}
 		});
 	};
-	scrollWindow();
+	// scrollWindow();
 
 	
 
